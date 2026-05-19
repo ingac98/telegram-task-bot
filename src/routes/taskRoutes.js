8b6d144ce09fa6164ca0ba93a9883e55c1a918f4
ingac98 <ingajorly@gmail.com>
2026-05-19 16:00:19 -0500

    feat: Completar endpoints de tareas

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -8,4 +8,16 @@ router.post('/', taskController.createTask);
 
 router.get('/', taskController.getTasks);
 
+router.get('/overdue', taskController.getOverdueTasks);
+
+router.get('/:id', taskController.getTaskById);
+
+router.patch('/:id', taskController.updateTask);
+
+router.patch('/:id/complete', taskController.completeTask);
+
+router.patch('/:id/postpone', taskController.postponeTask);
+
+router.delete('/:id', taskController.deleteTask);
+
 module.exports = router;
--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,4 +1,12 @@
+const mongoose = require('mongoose');
+
 const { Task, TASK_STATUS } = require('../models/Task');
+
+const validateObjectId = (id) => {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new Error('ID de tarea no válido');
+  }
+};
 
 const createTask = async (taskData) => {
   const { telegramUserId, title, description, dueDate } = taskData;
@@ -45,7 +53,164 @@ const getTasks = async (filters = {}) => {
   return tasks;
 };
 
+const getTaskById = async (id) => {
+  validateObjectId(id);
+
+  const task = await Task.findOne({
+    _id: id,
+    status: { $ne: TASK_STATUS.DELETED },
+  });
+
+  if (!task) {
+    throw new Error('Tarea no encontrada');
+  }
+
+  return task;
+};
+
+const getOverdueTasks = async (filters = {}) => {
+  const now = new Date();
+
+  const query = {
+    status: TASK_STATUS.PENDING,
+    dueDate: { $ne: null, $lt: now },
+  };
+
+  if (filters.telegramUserId) {
+    query.telegramUserId = filters.telegramUserId;
+  }
+
+  const tasks = await Task.find(query).sort({
+    dueDate: 1,
+  });
+
+  return tasks;
+};
+
+const updateTask = async (id, updateData) => {
+  validateObjectId(id);
+
+  const allowedFields = ['title', 'description', 'dueDate'];
+  const dataToUpdate = {};
+
+  allowedFields.forEach((field) => {
+    if (Object.prototype.hasOwnProperty.call(updateData, field)) {
+      dataToUpdate[field] = updateData[field];
+    }
+  });
+
+  if (dataToUpdate.title !== undefined && !dataToUpdate.title) {
+    throw new Error('El título no puede estar vacío');
+  }
+
+  if (dataToUpdate.dueDate !== undefined) {
+    dataToUpdate.reminderSent = false;
+  }
+
+  const task = await Task.findOneAndUpdate(
+    {
+      _id: id,
+      status: { $ne: TASK_STATUS.DELETED },
+    },
+    dataToUpdate,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!task) {
+    throw new Error('Tarea no encontrada');
+  }
+
+  return task;
+};
+
+const completeTask = async (id) => {
+  validateObjectId(id);
+
+  const task = await Task.findOneAndUpdate(
+    {
+      _id: id,
+      status: TASK_STATUS.PENDING,
+    },
+    {
+      status: TASK_STATUS.COMPLETED,
+      completedAt: new Date(),
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!task) {
+    throw new Error('Tarea no encontrada o ya completada');
+  }
+
+  return task;
+};
+
+const postponeTask = async (id, newDueDate) => {
+  validateObjectId(id);
+
+  if (!newDueDate) {
+    throw new Error('La nueva fecha es obligatoria');
+  }
+
+  const task = await Task.findOneAndUpdate(
+    {
+      _id: id,
+      status: TASK_STATUS.PENDING,
+    },
+    {
+      dueDate: newDueDate,
+      reminderSent: false,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!task) {
+    throw new Error('Tarea no encontrada o no está pendiente');
+  }
+
+  return task;
+};
+
+const deleteTask = async (id) => {
+  validateObjectId(id);
+
+  const task = await Task.findOneAndUpdate(
+    {
+      _id: id,
+      status: { $ne: TASK_STATUS.DELETED },
+    },
+    {
+      status: TASK_STATUS.DELETED,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!task) {
+    throw new Error('Tarea no encontrada');
+  }
+
+  return task;
+};
+
 module.exports = {
   createTask,
   getTasks,
+  getTaskById,
+  getOverdueTasks,
+  updateTask,
+  completeTask,
+  postponeTask,
+  deleteTask,
 };
